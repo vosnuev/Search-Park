a2c9443a7944f77d1a42ec009a80b5587e746bdd
92shepherd <shepherdpkw@gmail.com>
2026-03-17 19:08:30 +0900

    Fix: 운영시간, 결제 정보 적재

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,5 +1,9 @@
 from process.address import importRawAddress
 from process.parking_lot import load_parking_lot
+from process.operation_time import run as op_run
+from process.payment import run as pay_run
 
 importRawAddress()
 load_parking_lot()
+op_run('./src/data/전국주차장정보표준데이터_1.csv')
+pay_run('./src/data/전국주차장정보표준데이터_1.csv')
